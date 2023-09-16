dedf5051612c720be625ff10c72a8e611c0cdd6b
codeabuu <abdulngui718@gmail.com>
2023-09-16 11:25:46 -0700

    updates

---
+++ setup_mysql_test.sql
@@ -0,0 +1,15 @@
+-- Create project testing database with the name: hbnb_test_db
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- Create a new user named: hbnb_test with the password: hbnb_test_pwd if it doesn't exist
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- Granting the SELECT privilege for the user hbnb_test on the db performance_schema
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+
+-- Grant all privileges to the new user on hbnb_test_db
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+
+-- Flush privileges to apply changes
+FLUSH PRIVILEGES;
+
