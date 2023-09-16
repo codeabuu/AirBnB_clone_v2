2b45be87071e6e266abeea010191ba96754e5242
codeabuu <abdulngui718@gmail.com>
2023-09-16 14:39:59 -0700

    updates

--- models/amenity.py
+++ models/amenity.py
@@ -8,7 +8,5 @@ from sqlalchemy import Column, String
 class Amenity(BaseModel, Base):
     '''amenity class'''
     __tablename__ = 'amenities'
-    if storage_type == 'db':
-        name = Column(String(128), nullable=False)
-    else:
-        name = ""
+
+    name = Column(String(128), nullable=False) if storage_type == 'db' else ""
