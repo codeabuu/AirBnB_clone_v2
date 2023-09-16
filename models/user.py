6d0d94b8b9c6abe0760caf686bc2bfa7046391e2
codeabuu <abdulngui718@gmail.com>
2023-09-16 14:25:18 -0700

    updates

--- models/user.py
+++ models/user.py
@@ -9,17 +9,12 @@ from sqlalchemy.orm import relationship
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
+
+    email = Column(String(128), nullable=False) if storage_type == 'db' else ""
+    password = Column(String(128), nullable=False) if storage_type == 'db' else ""
+    first_name = Column(String(128), nullable=True) if storage_type == 'db' else ""
+    last_name = Column(String(128), nullable=True) if storage_type == 'db' else ""
+
     if storage_type == 'db':
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship('Place', backref='user',
-                              cascade='all, delete, delete-orphan')
-        reviews = relationship('Review', backref='user',
-                               cascade='all, delete, delete-orphan')
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+        places = relationship('Place', backref='user', cascade='all, delete, delete-orphan')
+        reviews = relationship('Review', backref='user', cascade='all, delete, delete-orphan')
