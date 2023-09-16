23aefad400df1bd535a8240cc5dcfb8148a3a626
codeabuu <abdulngui718@gmail.com>
2023-09-16 13:21:17 -0700

    updates

--- models/base_model.py
+++ models/base_model.py
@@ -32,14 +32,13 @@ class BaseModel:
         """Instatntiates a new model"""
         if not kwargs:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.created_at = self.updated_at = datetime.now()
         else:
-            for k in kwargs:
+            for k, v in kwargs.items():
                 if k in ['created_at', 'updated_at']:
-                    setattr(self, k, datetime.fromisoformat(kwargs[k]))
+                    setattr(self, k, datetime.fromisoformat(v]))
                 elif k != '__class__':
-                    setattr(self, k, kwargs[k])
+                    setattr(self, k, v)
             if storage_type == 'db':
                 if not hasattr(kwargs, 'id'):
                     setattr(self, 'id', str(uuid.uuid4()))
