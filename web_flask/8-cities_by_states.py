95eadd10e972cfa3957a0284196c914248699b75
codeabuu <abdulngui718@gmail.com>
2023-10-21 13:36:07 -0700

    udpates

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -3,7 +3,7 @@
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-
+from models.city import City
 
 app = Flask(__name__)
 
@@ -18,7 +18,8 @@ def close(self):
 def cities_by_states():
     """Displays a html page with states and cities"""
     states = storage.all(State)
-    return render_template('8-cities_by_states.html', states=states)
+    cities = storage.all(City)
+    return render_template('8-cities_by_states.html', states=states, cities=cities)
 
 
 if __name__ == '__main__':
