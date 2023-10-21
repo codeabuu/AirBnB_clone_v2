6124825cf6b537faa93ead86962999ff97a6cd5d
codeabuu <abdulngui718@gmail.com>
2023-10-21 13:12:56 -0700

    updates

--- models/user.py
+++ models/user.py
@@ -1,38 +1,46 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
+""" holds class User"""
+import hashlib
 from models.base_model import BaseModel, Base
-from models import storage_type
-from sqlalchemy import Column, String
+from os import getenv
 from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
 
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
-    __tablename__ = 'users'
+    """Representation of a user """
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'users'
+        email = Column(String(128),
+                       nullable=False)
+        _password = Column('password',
+                           String(128),
+                           nullable=False)
+        first_name = Column(String(128),
+                            nullable=True)
+        last_name = Column(String(128),
+                           nullable=True)
+        places = relationship("Place",
+                              backref="user",
+                              cascade="all, delete-orphan")
+        reviews = relationship("Review",
+                               backref="user",
+                               cascade="all, delete-orphan")
+    else:
+        email = ""
+        _password = ""
+        first_name = ""
+        last_name = ""
 
-    email = (
-            Column(String(128), nullable=False)
-            if storage_type == 'db'
-            else ""
-            )
-    password = (
-            Column(String(128), nullable=False)
-            if storage_type == 'db'
-            else ""
-            )
-    first_name = (
-            Column(String(128), nullable=True)
-            if storage_type == 'db'
-            else ""
-            )
-    last_name = (
-            Column(String(128), nullable=True)
-            if storage_type == 'db'
-            else ""
-            )
+    def __init__(self, *args, **kwargs):
+        """initializes user"""
+        super().__init__(*args, **kwargs)
 
-    if storage_type == 'db':
-        places = relationship(
-                'Place', backref='user', cascade='all, delete, delete-orphan')
-        reviews = relationship(
-                'Review', backref='user', cascade='all, delete, delete-orphan')
+    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, pwd):
+        """hashing password values"""
+        self._password = pwd
